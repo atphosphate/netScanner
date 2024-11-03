1070798ce784bb9777a00817135e55a6946a4153
atphosphate <cagritokur@gmail.com>
2024-11-03 16:36:39 -0500

    Initial commit

---
+++ net-scanner.py
@@ -0,0 +1,24 @@
+import scapy.all as scapy
+import optparse
+
+#1)arp_request
+#2)broadcast
+#3)response
+
+def get_info():
+    parse_object = optparse.OptionParser()
+    parse_object.add_option("-r","--range", dest="ip_adress", help="Range of local IP addresses")
+    (user_input, arguments) = parse_object.parse_args()
+    return user_input
+
+def scan(ip_range):
+    arp_request_packet = scapy.ARP(pdst=ip_range)
+    #scapy.ls(scapy.ARP())
+    arp_broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    #scapy.ls(scapy.Ether())
+    combined_packet = arp_broadcast_packet/arp_request_packet
+    return scapy.srp(combined_packet, timeout=1)
+
+(user_input, arguments) = get_info()
+(answered_list, unanswered_list) = scan(user_input.ip_adress)
+answered_list.summary()
